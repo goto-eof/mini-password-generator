4c044802176dee672cf8bad7f950a629b8f69fd3
Andrei Dodu <andrei.dodu@hotmail.it>
2023-01-28 19:39:50 +0100

    feat: password entropy

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use gtk4::prelude::{ApplicationExt, ApplicationExtManual};
 use gtk4::traits::{
     ButtonExt, CheckButtonExt, EditableExt, GridExt, GtkWindowExt, RangeExt, WidgetExt,
 };
-use password_generator::core::generate_password;
+use gtk4::Label;
+use password_generator::core::{calculate_entropy, generate_password};
 pub mod password_generator;
 
 fn main() {
@@ -35,7 +36,9 @@ fn build_ui(application: &gtk4::Application) {
     window.set_child(Some(&grid));
 
     let password_field = gtk4::Entry::builder().placeholder_text("").build();
-    password_field.set_text(generate_password(20, true, true, true, true).as_str());
+    let generated_password = generate_password(20, true, true, true, true);
+    password_field.set_text(generated_password.0.as_str());
+
     grid.attach(&password_field, 0, 0, 1, 1);
 
     let upper_case = gtk4::CheckButton::with_label("Upper case letters");
@@ -56,24 +59,14 @@ fn build_ui(application: &gtk4::Application) {
     grid.attach(&length_box, 0, 1, 1, 1);
 
     let range = gtk4::Scale::with_range(gtk4::Orientation::Horizontal, 1.0, 32.0, 1.0);
-    range.set_value(20.0);    
+    range.set_value(20.0);
     grid.attach(&range, 1, 1, 2, 1);
     range.connect_value_changed(clone!(@weak length_box=>move |range| {
         length_box.set_text( format!("{}",range.value().trunc()).as_str());
     }));
 
     let generate_button = gtk4::Button::with_label("Generate");
-    generate_button.connect_clicked(
-        clone!(@weak length_box, @weak clipboard, @weak upper_case,  @weak lower_case,  @weak numbers,  @weak  symbols, @weak password_field => move |_btn| {
-       let length_opt = length_box.text().parse::<i32>();
-let mut length = 10;
-       if length_opt.is_ok(){
-    length = length_opt.unwrap();
-}
-       
-            password_field.set_text(generate_password(length, upper_case.is_active(), lower_case.is_active(), numbers.is_active(), symbols.is_active()).as_str());
-               }),
-    );
+
     grid.attach(&generate_button, 1, 0, 1, 1);
 
     let copy_button = gtk4::Button::with_label("Copy");
@@ -90,5 +83,43 @@ let mut length = 10;
 
     grid.attach(&quit_button, 0, 6, 3, 1);
 
+    let password_length_opt = length_box.text().parse::<i32>();
+    let mut password_length = 20;
+    if password_length_opt.is_ok() {
+        password_length = password_length_opt.unwrap();
+    }
+    let title = gtk4::Label::builder()
+        .label(
+            format!(
+                "Password Entropy: {}",
+                calculate_entropy(password_length, 100).trunc()
+            )
+            .as_str(),
+        )
+        .halign(gtk4::Align::Start)
+        .build();
+    title.add_css_class("title-2");
+
+    grid.attach(&title, 1, 2, 1, 1);
+
+    update_entropy_label(&title, generated_password.1);
+
+    generate_button.connect_clicked(
+        clone!(@weak title, @weak length_box, @weak clipboard, @weak upper_case,  @weak lower_case,  @weak numbers,  @weak  symbols, @weak password_field => move |_btn| {
+       let length_opt = length_box.text().parse::<i32>();
+let mut length = 10;
+       if length_opt.is_ok(){
+    length = length_opt.unwrap();
+}
+       let generated_password = generate_password(length, upper_case.is_active(), lower_case.is_active(), numbers.is_active(), symbols.is_active());
+            password_field.set_text(generated_password.0.as_str());
+            update_entropy_label(&title, generated_password.1);
+               }),
+    );
+
     window.show();
+}
+
+fn update_entropy_label(label: &Label, entropy: f64) {
+    label.set_label(format!("Password Entropy: {}", entropy.trunc()).as_str());
 }
--- src/password_generator/core.rs
+++ src/password_generator/core.rs
@@ -3,9 +3,9 @@ use std::fmt::UpperExp;
 use rand::distributions::Uniform;
 use rand::prelude::Distribution;
 
-pub fn generate_password(length: i32, uc: bool, lc: bool, num: bool, sym: bool) -> String {
+pub fn generate_password(length: i32, uc: bool, lc: bool, num: bool, sym: bool) -> (String, f64) {
     if !uc && !lc && !num && !sym {
-        return "".to_owned();
+        return ("".to_owned(), 0.0);
     }
 
     let low_case = "abcdefghijklmnopqrstuvxyz";
@@ -40,5 +40,11 @@ pub fn generate_password(length: i32, uc: bool, lc: bool, num: bool, sym: bool) 
         let char = char.nth(throw).unwrap();
         password = format!("{}{}", password, char);
     }
-    return password;
+    return (password, calculate_entropy(length, all.len().into()));
+}
+
+pub fn calculate_entropy(password_length: i32, charset_length: usize) -> f64 {
+    let charset_length: f64 = charset_length as f64;
+    let password_length: f64 = password_length.into();
+    return password_length * charset_length.log2() / 2.0_f64.log2();
 }
